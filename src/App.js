abf5d48a320321f9545cc979e0739fe6765ba5ef
Mikael <mikaelangelopizzi@gmail.com>
2022-09-12 10:19:22 -0400

    [FEAT] Add form component]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { Fragment } from "react";
+import Form from "./components/Form";
 
 function App() {
   return (
     <Fragment>
       <h1>Patient Manager</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Form />
+          </div>
+          <div className="one-half column">
+            2
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+
+const Form = (props) => {
+    return ( 
+    <h1>
+        From form
+    </h1> );
+}
+ 
+export default Form;
