0fa052ac78d9618d10f7d5b352e4259cb9033362
Mikael <mikaelangelopizzi@gmail.com>
2022-09-12 10:44:59 -0400

    [ADD] Create form fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,55 @@
 import React, { Fragment } from "react";
 
 const Form = (props) => {
-    return ( 
-    <h1>
-        From form
-    </h1> );
+	return ( 
+		<Fragment>
+      <h2>Create Appointment</h2>
+      <form>
+        <label>Pet's Name</label>
+        <input
+          type="text"
+          name="pet"
+          className="u-full-width"
+          placeholder="Pet's Name"
+        />
+
+        <label>Owner's Name</label>
+        <input
+          type="text"
+          name="owner"
+          className="u-full-width"
+          placeholder="Pet Owner's Name"
+        />
+
+        <label>Date</label>
+        <input
+          type="date"
+          name="date"
+          className="u-full-width"
+        />
+
+        <label>Check In Time</label>
+        <input
+          type="time"
+          name="time"
+          className="u-full-width"
+        />
+
+        <label>Symptoms</label>
+        <textarea
+          className="u-full-width"
+          name="symptoms"
+        >
+        </textarea>
+
+        <button
+          type="submit"
+          className="u-full-width button-primary"
+        >Add Appointment
+        </button>
+      </form>
+		</Fragment>
+	);
 }
  
 export default Form;
